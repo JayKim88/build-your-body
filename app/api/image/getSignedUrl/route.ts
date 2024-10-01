bc6a924ca925f8e491f023ea3912a9f88cd4a7ee
Yongjae Kim <mearak43@gmail.com>
2024-10-01 21:24:17 +0900

    fix service account error in vercel

--- app/api/image/getSignedUrl/route.ts
+++ app/api/image/getSignedUrl/route.ts
@@ -12,12 +12,6 @@ const storage = new Storage({
   },
 });
 
-console.log("check11111", process.env.GOOGLE_CLOUD_SERVICE_CLIENT_EMAIL);
-console.log(
-  "check2222",
-  (process.env.GOOGLE_CLOUD_SERVICE_PRIVATE_KEY ?? "").replace(/\\n/g, "\n")
-);
-
 const bucketName = "program-complete-image";
 
 export async function GET(req: NextRequest) {
@@ -43,14 +37,10 @@ export async function GET(req: NextRequest) {
       expires: Date.now() + 15 * 60 * 1000, // 15 minutes
     };
 
-    console.log("storage?", storage);
-
     const [signedUrl] = await storage
       .bucket(bucketName)
       .file(imageName)
       .getSignedUrl(options);
-
-    console.log("signedUrl?", signedUrl);
 
     const completedUrl = `https://storage.googleapis.com/${bucketName}/${encodeURIComponent(
       imageName
